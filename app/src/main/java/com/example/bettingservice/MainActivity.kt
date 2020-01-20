4066ad5cd6f7e72643ea89eb0ffe3e4f5f06340d
허경 <gjrud1470@kaist.ac.kr>
2020-01-20 16:07:08 +0900

    start advertiseing from hostroomactivity'

--- app/src/main/java/com/example/bettingservice/MainActivity.kt
+++ app/src/main/java/com/example/bettingservice/MainActivity.kt
@@ -79,24 +79,6 @@ class MainActivity : AppCompatActivity() {
                         }
 
                         else {
-                            val options =
-                                AdvertisingOptions.Builder().setStrategy(Strategy.P2P_CLUSTER)
-                                    .build()
-
-                            Nearby.getConnectionsClient(this)
-                                .startAdvertising(
-                                    userName,
-                                    "com.example.bettingservice",
-                                    connCallback,
-                                    options
-                                )
-                                .addOnSuccessListener {
-                                    Log.wtf(TAG, "success")
-                                }
-                                .addOnFailureListener {
-                                    Log.wtf(TAG, "failue")
-                                }
-
                             //Start Receiving Player Activity
                             startActivity(Intent(this, HostRoomActivity::class.java))
                         }
@@ -117,72 +99,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, RoomsActivity::class.java))
             finish()
         }
-    }
-
-    val connCallback = object : ConnectionLifecycleCallback() {
-
-        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
-            AlertDialog.Builder(this@MainActivity)
-                .setTitle(connectionInfo.endpointName + "님과 연결하겠습니까?")
-                .setMessage("토큰이 일치하는지 확인해주세요\n" + connectionInfo.authenticationToken)
-                .setPositiveButton("연결") { _, _ ->
-                    Nearby.getConnectionsClient(this@MainActivity)
-                        .acceptConnection(endpointId, payloadCallback)
-                }
-                .setNegativeButton("거부") { _, _ ->
-                    Nearby.getConnectionsClient(this@MainActivity)
-                        .rejectConnection(endpointId)
-                }
-                .show()
-        }
-
-        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
-            Nearby.getConnectionsClient(this@MainActivity).stopDiscovery()
-
-            when (result.status.statusCode) {
-                ConnectionsStatusCodes.STATUS_OK -> {
-                }
-                ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
-                    Log.wtf(TAG, "rejected")
-                }
-                ConnectionsStatusCodes.STATUS_ERROR -> {
-                }
-                else -> {
-                }
-            }
-        }
-
-        override fun onDisconnected(endpointId: String) {
-            Log.wtf(TAG, "disconnected")
-        }
-
-    }
-
-    val discoverCallback = object : EndpointDiscoveryCallback() {
-        override fun onEndpointFound(
-            endpointId: String,
-            discoveredEndpointInfo: DiscoveredEndpointInfo
-        ) {
-            Log.wtf(TAG, "check endpoint ${endpointId}")
-            Nearby.getConnectionsClient(this@MainActivity)
-                .requestConnection(userName, endpointId, connCallback)
-        }
-
-        override fun onEndpointLost(endpointId: String) {
-            Log.wtf(TAG, "check endpoint lost")
-        }
-
-    }
-
-    val payloadCallback = object : PayloadCallback() {
-        override fun onPayloadReceived(endpointId: String, payload: Payload) {
-
-        }
-
-        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
-
-        }
-
     }
 
 }
